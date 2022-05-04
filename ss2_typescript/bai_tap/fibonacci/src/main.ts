fbf99aa0408207e241d92930fb8a9bc18683d018
haupham96 <hau.pv.0596@gmail.com>
2022-05-04 21:15:54 +0700

    update bt ss2

--- ss2_typescript/bai_tap/fibonacci/src/main.ts
+++ ss2_typescript/bai_tap/fibonacci/src/main.ts
@@ -1,18 +1,18 @@
-// function sumFibonacci(n1: number, n2: number): number {
-//     let total: number = 0;
-//     let sum: number = 0;
-//     for (let i = 0; i < 10; i++) {
-//
-//         sum = n1 + n2;
-//         n1 = n2;
-//         n2 = sum;
-//         total += n1;
-//
-//     }
-//     return total;
-// }
-//
-// console.log("total : " + sumFibonacci(0, 1));
+function sumFibonacci(n1: number, n2: number, count: number): number {
+    let total: number = 0;
+    let sum: number = 0;
+    for (let i = 0; i < count; i++) {
+
+        total = n1 + n2;
+        n1 = n2;
+        n2 = sum;
+        sum += n1;
+
+    }
+    return sum;
+}
+
+console.log("total : " + sumFibonacci(0, 1, 10));
 
 function fibonacci(num: number): number {
 
@@ -23,4 +23,4 @@ function fibonacci(num: number): number {
     return fibonacci(num - 1) + fibonacci(num - 2);
 }
 
-console.log(fibonacci(10));
+// console.log(fibonacci(10));
